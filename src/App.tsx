5f8ffa4be939ad0f9d4eeca751f8a755df549eae
Vladimir Shefer <quameu@gmail.com>
2025-10-25 02:43:02 +0200

    Cleanup template

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
@@ -28,6 +23,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+        App
     </>
   )
 }
