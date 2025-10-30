e43525af178d7708608813129d84c9bdc3f921fc
Vladimir Shefer <quameu@gmail.com>
2025-10-30 02:49:50 +0100

    Hordes: extract constants.ts + balance

--- src/pages/games/Hordes/pickups.ts
+++ src/pages/games/Hordes/pickups.ts
@@ -2,13 +2,10 @@ import Phaser from 'phaser'
 import {healHero} from './hero.ts'
 import type {HeroState} from './types'
 import {XpCrystalManager} from './xpCrystals.ts'
-import {
-  ENEMY_SPRITESHEET_KEY,
-  HEAL_POTION_FRAME_INDEX,
-  PORTAL_OPENS_FRAME_INDEX,
-} from './sprite.ts'
+import {ENEMY_SPRITESHEET_KEY, HEAL_POTION_FRAME_INDEX, PORTAL_OPENS_FRAME_INDEX} from './sprite.ts'
+import {HERO_BASE_HP, HERO_BASE_RADIUS, MOB_BASE_RADIUS} from "./game/constants.ts";
 
-export const PICKUP_DEFAULT_SIZE = 28
+export const PICKUP_DEFAULT_SIZE = HERO_BASE_RADIUS * 2
 
 interface PickupHooks {
   getHero(): HeroState
@@ -16,10 +13,10 @@ interface PickupHooks {
   onMagnetActivated(): void
 }
 
-const HEAL_AMOUNT = 30
-const MAGNET_FLASH = { duration: 140, r: 120, g: 200, b: 255 }
+const HEAL_AMOUNT = HERO_BASE_HP * 0.3
+const MAGNET_FLASH = { duration: 100, r: 120, g: 200, b: 255 }
 const HEAL_FLASH = { duration: 100, r: 80, g: 180, b: 255 }
-const SPAWN_MARGIN = 60
+const SPAWN_MARGIN = MOB_BASE_RADIUS * 3
 
 export class PickupManager {
   private scene: Phaser.Scene
@@ -42,16 +39,13 @@ export class PickupManager {
   }
 
   spawnHealPack(view: Phaser.Geom.Rectangle) {
-    const hero = this.hooks.getHero()
-    if (hero.hp >= hero.maxHp) return
-
     const x = Phaser.Math.FloatBetween(view.left + SPAWN_MARGIN, view.right - SPAWN_MARGIN)
     const y = Phaser.Math.FloatBetween(view.top + SPAWN_MARGIN, view.bottom - SPAWN_MARGIN)
 
     const diameter = PICKUP_DEFAULT_SIZE
     const pack = this.scene.add.image(x, y, ENEMY_SPRITESHEET_KEY, HEAL_POTION_FRAME_INDEX)
     pack.setDisplaySize(diameter, diameter)
-    pack.setTint(0xff7603)
+    pack.setTint(0xf06040)
     pack.setData('radius', diameter / 2)
     this.healPacks.push(pack)
   }
