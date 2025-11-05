f01ab000f05031c00025b6efe79fcf6fca2c348a
Vladimir Shefer <quameu@gmail.com>
2025-11-05 02:58:10 +0100

    Hordes: Balance: speed of hero and mobs

--- src/pages/games/Hordes/game/constants.ts
+++ src/pages/games/Hordes/game/constants.ts
@@ -1,9 +1,9 @@
 export const WORLD_BOUNDS = 20000
-export const HERO_BASE_SPEED = 220
+export const HERO_BASE_SPEED = 150
 export const HERO_BASE_RADIUS = 14
 export const HERO_BASE_HP = 100
 export const HERO_BASE_DAMAGE = 5
-export const MOB_BASE_SPEED = HERO_BASE_SPEED * 0.5
+export const MOB_BASE_SPEED = HERO_BASE_SPEED * 0.6
 export const MOB_BASE_RADIUS = HERO_BASE_RADIUS
 export const MOB_BASE_HP = 5
 export const MOB_BASE_DAMAGE = HERO_BASE_HP * 0.05
